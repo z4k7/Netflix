3e5ac7660c8344327c2fbf866f7d65704dc33df2
Sachin Kizhakkepurath <sachinkp1997@gmail.com>
2023-11-03 11:33:54 +0530

    netflix commit

---
+++ src/app/pages/search/search.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import{FormControl,FormGroup} from '@angular/forms'
+import { MovieApiServiceService } from 'src/app/service/movie-api-service.service';
+
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css']
+})
+export class SearchComponent {
+
+constructor(private service:MovieApiServiceService){}
+
+ngOnInit(): void{
+
+}
+
+searchResult:any
+
+searchForm = new FormGroup({
+  'movieName': new FormControl(null)
+})
+
+submitForm(){
+  console.log(this.searchForm.value,'searchform#');
+  this.service.getSearchMovie(this.searchForm.value).subscribe((result)=>{
+    console.log(result,'searchmovie##');
+    this.searchResult=result.results
+    
+  }) 
+}
+
+}
+
